db4c414c9a2f7a8fb7eda144c1837580f6a19020
Sean Ervinson <ervinsonong@gmail.com>
2020-01-08 15:52:47 +0800

    Added model for location and tag

---
+++ app/src/main/java/com/sktt1/butters/data/models/Tag.java
@@ -0,0 +1,68 @@
+package com.sktt1.butters.data.models;
+
+import androidx.annotation.NonNull;
+
+import java.util.Date;
+
+public class Tag {
+    private String id;
+    private String name;
+    private String mac;
+    private Location lastSeenLocation;
+    private String macAddress;
+    private Date lastSeenTime;
+
+    public Location getLastSeenLocation() {
+        return lastSeenLocation;
+    }
+
+    public void setLastSeenLocation(Location lastSeenLocation) {
+        this.lastSeenLocation = lastSeenLocation;
+    }
+
+    public Date getLastSeenTime() {
+        return lastSeenTime;
+    }
+
+    public void setLastSeenTime(Date lastSeenTime) {
+        this.lastSeenTime = lastSeenTime;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMac() {
+        return mac;
+    }
+
+    public void setMac(String mac) {
+        this.mac = mac;
+    }
+
+    public String getMacAddress() {
+        return macAddress;
+    }
+
+    public void setMacAddress(String macAddress) {
+        this.macAddress = macAddress;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return name;
+    }
+}
