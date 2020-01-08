304cdb9ea0db8c8e857a4d90c2c9242074e1e4f4
Sean Ervinson <ervinsonong@gmail.com>
2020-01-08 16:57:06 +0800

    Separated into own file

---
+++ app/src/main/java/com/sktt1/butters/data/utilities/DateTimePattern.java
@@ -0,0 +1,18 @@
+package com.sktt1.butters.data.utilities;
+
+public enum DateTimePattern{
+    FULL_DATETIME("yyyy-mm-dd hh:mm:ss"),
+    TIME("hh:mm a"),
+    DATE("M dd,yyyy"),
+    SHORT_DATETIME("yyyy-mm-dd hh:mm");
+
+    private  String action;
+
+    public String getAction(){
+        return this.action;
+    }
+
+    DateTimePattern(String action){
+        this.action = action;
+    }
+}
--- app/src/main/java/com/sktt1/butters/data/utilities/DateUtility.java
+++ app/src/main/java/com/sktt1/butters/data/utilities/DateUtility.java
@@ -4,23 +4,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public enum DateTimePattern{
-    FULL_DATETIME("yyyy-mm-dd hh:mm:ss"),
-    TIME("hh:mm"),
-    DATE("yyyy-mm-dd"),
-    SHORT_DATETIME("yyyy-mm-dd hh:mm");
-
-    private  String action;
-
-    public String getAction(){
-        return this.action;
-    }
-
-    DateTimePattern(String action){
-        this.action = action;
-    }
-}
-
 public class DateUtility {
     public static String getFormattedDate(Date date,DateTimePattern pattern){
         SimpleDateFormat formatter = new SimpleDateFormat(pattern.getAction(), Locale.ENGLISH);
