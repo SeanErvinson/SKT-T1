3035ccb61cec9828765ab6b897a189fea74d9366
Sean Ervinson <ervinsonong@gmail.com>
2020-01-14 15:19:44 +0800

    Implemented fragment manager and fragments

--- app/src/main/java/com/sktt1/butters/MainActivity.java
+++ app/src/main/java/com/sktt1/butters/MainActivity.java
@@ -1,13 +1,82 @@
 package com.sktt1.butters;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
-public class MainActivity extends AppCompatActivity {
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.sktt1.butters.data.fragments.HomeFragment;
+import com.sktt1.butters.data.fragments.MapFragment;
+import com.sktt1.butters.data.fragments.OnFragmentInteractionListener;
+import com.sktt1.butters.data.fragments.SettingsFragment;
+
+public class MainActivity extends AppCompatActivity implements OnFragmentInteractionListener {
+    private static final String TAG = "MainActivity";
+
+    private BottomNavigationView mBottomNavigationView;
+    private FrameLayout mMainContainer;
+    private Fragment mHomeFragment, mMapFragment, mSettingsFragment;
+    private Fragment currentFragment;
+    private FragmentManager mFragmentManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeWidget();
+        initializeFragments();
+
+        mFragmentManager.beginTransaction().add(R.id.fl_main_container, mHomeFragment, HomeFragment.TAG).hide(mSettingsFragment).commit();
+        mFragmentManager.beginTransaction().add(R.id.fl_main_container, mMapFragment, MapFragment.TAG).hide(mMapFragment).commit();
+        mFragmentManager.beginTransaction().add(R.id.fl_main_container,mSettingsFragment, SettingsFragment.TAG).commit();
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        mFragmentManager.beginTransaction().hide(currentFragment).show(mHomeFragment).commit();
+                        currentFragment = mHomeFragment;
+                        return true;
+
+                    case R.id.navigation_map:
+                        mFragmentManager.beginTransaction().hide(currentFragment).show(mMapFragment).commit();
+                        currentFragment = mMapFragment;
+                        return true;
+
+                    case R.id.navigation_settings:
+                        mFragmentManager.beginTransaction().hide(currentFragment).show(mSettingsFragment).commit();
+                        currentFragment = mSettingsFragment;
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void initializeWidget(){
+        mMainContainer = findViewById(R.id.fl_main_container);
+        mBottomNavigationView = findViewById(R.id.bnv_main_navigation);
+    }
+
+    private void initializeFragments(){
+        mHomeFragment = new HomeFragment();
+        mMapFragment = new MapFragment();
+        mSettingsFragment = new SettingsFragment();
+        currentFragment = mHomeFragment;
+        mFragmentManager = getSupportFragmentManager();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
