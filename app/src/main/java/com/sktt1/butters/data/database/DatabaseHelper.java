ea8b0953e0e2aecbe4deb1689f6b573a42917171
Sean Ervinson <ervinsonong@gmail.com>
2020-01-17 17:44:33 +0800

    Added helper class for db

--- app/src/main/java/com/sktt1/butters/data/database/DatabaseHelper.java
+++ app/src/main/java/com/sktt1/butters/data/database/DatabaseHelper.java
@@ -1,4 +1,32 @@
 package com.sktt1.butters.data.database;
 
-public class DatabaseHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.sktt1.butters.data.database.tables.ActivityTable;
+import com.sktt1.butters.data.database.tables.LocationTable;
+import com.sktt1.butters.data.database.tables.TagTable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DB_NAME = "skt.db";
+    public static final int DB_VERSION=1;
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(LocationTable.getCreateQuery());
+        sqLiteDatabase.execSQL(TagTable.getCreateQuery());
+        sqLiteDatabase.execSQL(ActivityTable.getCreateQuery());
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
