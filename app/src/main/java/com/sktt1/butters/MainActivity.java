df6b322167dd89f6be6a3242a9a0f140b4b12b08
Sean Ervinson <ervinsonong@gmail.com>
2020-01-23 16:54:16 +0800

    Removed unused code

--- app/src/main/java/com/sktt1/butters/MainActivity.java
+++ app/src/main/java/com/sktt1/butters/MainActivity.java
@@ -7,7 +7,6 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -38,8 +37,6 @@ public class MainActivity extends AppCompatActivity implements OnFragmentInterac
         initializeActionBar();
         initializeWidget();
         initializeFragments();
-//        Intent intent = new Intent(this, PairTagActivity.class);
-//        startActivity(intent);
     }
 
     @Override
