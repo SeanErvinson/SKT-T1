635bb439fe09963940801be7968c7f799b41ae2b
Emmanuel <kyoske_tenchi@yahoo.com>
2020-01-26 23:16:13 +0800

    changed query to INTEGER PRIMARY KEY

--- app/src/main/java/com/sktt1/butters/data/database/tables/ActivityTable.java
+++ app/src/main/java/com/sktt1/butters/data/database/tables/ActivityTable.java
@@ -12,6 +12,6 @@ public class ActivityTable {
     public static final String COL_HAS_READ = "has_read";
 
     public static String getCreateQuery() {
-        return String.format("CREATE TABLE %s (%s INTEGER NOT NULL PRIMARY, %s TEXT NOT NULL, %s LONG NOT NULL, %s BOOLEAN NOT NULL)", TABLE, COL_ID, COL_MESSAGE, COL_NOTIFIED_ON, COL_HAS_READ);
+        return String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY, %s TEXT NOT NULL, %s LONG NOT NULL, %s BOOLEAN NOT NULL)", TABLE, COL_ID, COL_MESSAGE, COL_NOTIFIED_ON, COL_HAS_READ);
     }
 }
