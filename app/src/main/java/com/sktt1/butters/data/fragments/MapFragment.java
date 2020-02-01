8fda96f4828156233e1ce6dd2a2c12aaed7d34c2
Sean Ervinson <ervinsonong@gmail.com>
2020-02-01 23:01:32 +0800

    Added initial google map implementation

--- app/src/main/java/com/sktt1/butters/data/fragments/MapFragment.java
+++ app/src/main/java/com/sktt1/butters/data/fragments/MapFragment.java
@@ -1,31 +1,93 @@
 package com.sktt1.butters.data.fragments;
 
 import android.content.Context;
-import android.net.Uri;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.sktt1.butters.R;
+import com.sktt1.butters.data.models.Tag;
 
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
     public  static final String TAG = "MapFragment";
 
     private OnFragmentInteractionListener mListener;
+    private Button mBuzz, mSit;
+    private GoogleMap mMap;
 
     public MapFragment() {}
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initializeWidget(view);
+
+        mSit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addTag(new Tag(){{
+                    setName("Hello");
+                }});
+            }
+        });
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView =  inflater.inflate(R.layout.fragment_map, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        return rootView;
+    }
+
+    private void initializeWidget(View view){
+        mBuzz = view.findViewById(R.id.map_button_buzz);
+        mSit = view.findViewById(R.id.map_button_sit);
+    }
+
+
+    public void addTag(Tag tag){
+        MarkerOptions m = new MarkerOptions()
+                .position(new LatLng(37.4219999, -122.0862462))
+                .title(tag.getName())
+                .icon(bitmapDescriptorFromVector(getActivity(),R.drawable.ic_person_24px));
+        if(tag.getLastSeenLocationId() != 0){
+            m.snippet(String.valueOf(tag.getId()));
+        }
+        mMap.addMarker(m);
+    }
+
+
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
 
     @Override
     public void onAttach(Context context) {
@@ -42,5 +104,32 @@ public class MapFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        googleMap.clear();
+
+        CameraPosition googlePlex = CameraPosition.builder()
+                .target(new LatLng(37.4219999,-122.0862462))
+                .zoom(10)
+                .bearing(0)
+                .build();
+
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 1000, null);
+
+
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(37.4629101,-122.2449094))
+                .title("Iron Man")
+                .snippet("His Talent : Plenty of money"));
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(37.3092293,-122.1136845))
+                .title("Captain America"));
     }
 }
