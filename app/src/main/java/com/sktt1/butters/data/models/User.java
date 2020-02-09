e6552c7f87e0771dace752381ce667fb1e6dd1d2
Emmanuel <kyoske_tenchi@yahoo.com>
2020-02-09 21:56:41 +0800

    added User model

---
+++ app/src/main/java/com/sktt1/butters/data/models/User.java
@@ -0,0 +1,49 @@
+package com.sktt1.butters.data.models;
+
+public class User {
+    private String name;
+    private String nickname;
+    private int totalDevices;
+    private int activeDevices;
+    private int inactiveDevices;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getTotalDevices() {
+        return totalDevices;
+    }
+
+    public void setTotalDevices(int totalDevices) {
+        this.totalDevices = totalDevices;
+    }
+
+    public int getActiveDevices() {
+        return activeDevices;
+    }
+
+    public void setActiveDevices(int activeDevices) {
+        this.activeDevices = activeDevices;
+    }
+
+    public int getInactiveDevices() {
+        return inactiveDevices;
+    }
+
+    public void setInactiveDevices(int inactiveDevices) {
+        this.inactiveDevices = inactiveDevices;
+    }
+}
