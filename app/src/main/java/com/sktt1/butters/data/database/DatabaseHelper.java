c03ada5eea7e6d86a3ba5f9380441f64baafacf9
Emmanuel <kyoske_tenchi@yahoo.com>
2020-02-10 15:41:29 +0800

    fixed variable names

--- app/src/main/java/com/sktt1/butters/data/database/DatabaseHelper.java
+++ app/src/main/java/com/sktt1/butters/data/database/DatabaseHelper.java
@@ -44,16 +44,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     // Activity table interactions
-    public long activityCreateNotification(String message, String notified_on) {
+    public int activityCreateNotification(String message, String notifiedOn) {
         sqLiteDatabase = this.getWritableDatabase();
         ContentValues activityValues = new ContentValues();
         activityValues.put(ActivityTable.COL_MESSAGE, message);
-        activityValues.put(ActivityTable.COL_NOTIFIED_ON, notified_on);
+        activityValues.put(ActivityTable.COL_NOTIFIED_ON, notifiedOn);
         activityValues.put(ActivityTable.COL_HAS_READ, "false");
-        return sqLiteDatabase.insert(ActivityTable.TABLE, null, activityValues);
+        return (int) sqLiteDatabase.insert(ActivityTable.TABLE, null, activityValues);
     }
 
-    public void activityUpdateNotification( long id) {
+    public void activityUpdateNotification(int id) {
         sqLiteDatabase = this.getWritableDatabase();
         ContentValues activityValues = new ContentValues();
         activityValues.put(ActivityTable.COL_HAS_READ, "true");
@@ -68,46 +68,46 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     // Tag table interactions
 
-    public long tagCreateDevice(String name, String mac_address, String last_send_location_id, String last_seen_time, boolean is_connected){
+    public int tagCreateDevice(String name, String macAddress, int lastSendLocationId, String lastSeenTime, boolean isConnected){
 
         sqLiteDatabase = this.getWritableDatabase();
 
         ContentValues tagValues = new ContentValues();
 
         tagValues.put(TagTable.COL_NAME, name);
-        tagValues.put(TagTable.COL_MAC_ADDRESS, mac_address);
-        tagValues.put(TagTable.COL_LAST_SEEN_LOCATION_ID, last_send_location_id);
-        tagValues.put(TagTable.COL_LAST_SEEN_TIME, last_seen_time);
-        tagValues.put(TagTable.COL_IS_CONNECTED, Boolean.toString(is_connected));
+        tagValues.put(TagTable.COL_MAC_ADDRESS, macAddress);
+        tagValues.put(TagTable.COL_LAST_SEEN_LOCATION_ID, lastSendLocationId);
+        tagValues.put(TagTable.COL_LAST_SEEN_TIME, lastSeenTime);
+        tagValues.put(TagTable.COL_IS_CONNECTED, Boolean.toString(isConnected));
 
-        return sqLiteDatabase.insert(TagTable.TABLE, null, tagValues);
+        return (int) sqLiteDatabase.insert(TagTable.TABLE, null, tagValues);
     }
 
-    public void tagUpdateName(String name, long id){
+    public void tagUpdateName(String name, int id){
         sqLiteDatabase = this.getWritableDatabase();
         ContentValues tagValues = new ContentValues();
         tagValues.put(TagTable.COL_NAME, name);
-        sqLiteDatabase.update(TagTable.TABLE, tagValues, "id = ?", new String[] {Long.toString(id)});
+        sqLiteDatabase.update(TagTable.TABLE, tagValues, "id = ?", new String[] {Integer.toString(id)});
     }
 
-    public void tagUpdateLocation(long tagId, long locationId, String lastSeenTime, String longitude, String latitude){
+    public void tagUpdateLocation(int tagId, int locationId, String lastSeenTime, String longitude, String latitude){
         sqLiteDatabase = this.getWritableDatabase();
         ContentValues tagValues = new ContentValues();
         tagValues.put(TagTable.COL_LAST_SEEN_TIME, lastSeenTime);
-        sqLiteDatabase.update(TagTable.TABLE,  tagValues, "id = ?", new String[] {Long.toString(tagId)});
+        sqLiteDatabase.update(TagTable.TABLE,  tagValues, "id = ?", new String[] {Integer.toString(tagId)});
 
         ContentValues locationValues = new ContentValues();
         locationValues.put(LocationTable.COL_LATITUDE, latitude);
         locationValues.put(LocationTable.COL_LONGITUDE, longitude);
-        sqLiteDatabase.update(LocationTable.TABLE, locationValues, "id = ?", new String[] {Long.toString(locationId)});
+        sqLiteDatabase.update(LocationTable.TABLE, locationValues, "id = ?", new String[] {Integer.toString(locationId)});
     }
 
-    public void tagUpdateConnection(long id, boolean isConnected){
+    public void tagUpdateConnection(int id, boolean isConnected){
 
         sqLiteDatabase = this.getWritableDatabase();
         ContentValues tagValues = new ContentValues();
         tagValues.put(TagTable.COL_IS_CONNECTED, Boolean.toString(!isConnected));
-        sqLiteDatabase.update(TagTable.TABLE, tagValues,"id = ?", new String[] {Long.toString(id)});
+        sqLiteDatabase.update(TagTable.TABLE, tagValues,"id = ?", new String[] {Integer.toString(id)});
     }
 
     public Cursor tagFeedList(){
