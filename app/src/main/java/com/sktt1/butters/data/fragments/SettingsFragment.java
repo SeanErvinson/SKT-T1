b08b10bd852b1ed22b0f1a5553770807adb2ca33
Emmanuel <kyoske_tenchi@yahoo.com>
2020-02-10 15:23:19 +0800

    removed SettingsTagRecyclerAdapter

--- app/src/main/java/com/sktt1/butters/data/fragments/SettingsFragment.java
+++ app/src/main/java/com/sktt1/butters/data/fragments/SettingsFragment.java
@@ -1,10 +1,15 @@
 package com.sktt1.butters.data.fragments;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,8 +18,10 @@ import android.view.ViewGroup;
 import com.sktt1.butters.R;
 
 public class SettingsFragment extends Fragment {
-    public  static final String TAG = "SettingsFragment";
+    public static final String TAG = "SettingsFragment";
 
+
+    private RecyclerView mSettingsTagsView;
     private OnFragmentInteractionListener mListener;
 
     public SettingsFragment() {}
@@ -25,6 +32,16 @@ public class SettingsFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        initializeWidget(view);
+    }
+
+    private void initializeWidget(View view){
+        mSettingsTagsView = view.findViewById(R.id.rv_settings_tags_list);
+    }
 
     @Override
     public void onAttach(Context context) {
