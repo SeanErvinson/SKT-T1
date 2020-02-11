cfa0e3030cdc02adcc682022d3865d70411f0027
Sean Ervinson <ervinsonong@gmail.com>
2020-02-11 13:10:39 +0800

    Added string to date

--- app/src/main/java/com/sktt1/butters/data/utilities/DateUtility.java
+++ app/src/main/java/com/sktt1/butters/data/utilities/DateUtility.java
@@ -1,5 +1,6 @@
 package com.sktt1.butters.data.utilities;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -8,6 +9,10 @@ public class DateUtility {
     public static String getFormattedDate(Date date,DateTimePattern pattern){
         SimpleDateFormat formatter = new SimpleDateFormat(pattern.getAction(), Locale.ENGLISH);
         return formatter.format(date);
+    }
+    public static Date getStringDate(String value, DateTimePattern pattern) throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat(pattern.getAction(), Locale.ENGLISH);
+        return formatter.parse(value);
     }
 }
 
