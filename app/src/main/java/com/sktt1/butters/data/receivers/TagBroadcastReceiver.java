fc01800074e6e4feec6cb968f68f831de1461c97
Sean Ervinson <ervinsonong@gmail.com>
2020-02-15 00:35:23 +0800

    Created boilerplate for receiver

---
+++ app/src/main/java/com/sktt1/butters/data/receivers/TagBroadcastReceiver.java
@@ -0,0 +1,32 @@
+package com.sktt1.butters.data.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class TagBroadcastReceiver extends BroadcastReceiver {
+
+    public final static String ACTION_GATT_CONNECTED = "com.sktt1.butters.ACTION_GATT_CONNECTED";
+    public final static String ACTION_GATT_DISCONNECTED = "com.sktt1.butters.ACTION_GATT_DISCONNECTED";
+    public final static String ACTION_GATT_SERVICES_DISCOVERED = "com.sktt1.butters.ACTION_GATT_SERVICES_DISCOVERED";
+    public final static String ACTION_DATA_AVAILABLE = "com.sktt1.butters.ACTION_DATA_AVAILABLE";
+    public final static String EXTRA_DATA = "com.sktt1.butters.EXTRA_DATA";
+    public static final String ACTION_PHONE_ALERTED= "com.sktt1.butters.FMP_DATA";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final String action = intent.getAction();
+        if (ACTION_PHONE_ALERTED.equals(action)) {
+            // Send notification
+            // Play sound
+        } else if (ACTION_GATT_CONNECTED.equals(action)) {
+            // Send notification
+            // Get tag information through extra data
+        } else if (ACTION_GATT_DISCONNECTED.equals(action)) {
+            // Send notification
+            // Get tag information through extra data
+        } else if (ACTION_GATT_SERVICES_DISCOVERED.equals(action)) {
+        } else if (ACTION_DATA_AVAILABLE.equals(action)) {
+        }
+    }
+}
